1162768365900d2ab03e20a41c8d67a2e1801cd8
James Milne <s4b3r6@gmail.com>
2021-06-12 12:37:40 +1000

    Added directory support

--- README.md
+++ README.md
@@ -6,9 +6,11 @@ A tiny file watcher for Linux
 
 ## Usage
 
-	miniwatch FILE_TO_WATCH -c ARG ARG etc
+	miniwatch FILE_OR_DIR_TO_WATCH -c ARG ARG etc
 
 miniwatch will watch for the modified time of the given file, and when it detects a change in the modified time, it will run the given command and arguments.
+
+If it was given a directory, it will also watch for file creation & deletion to trigger the command. (Directories are checked recursively for files).
 
 For example:
 
@@ -16,9 +18,7 @@ For example:
 
 There are some caveats:
 
-+ If the given file to watch is actually a directory, some changes will be noticed and reacted to, but not all. This isn't a strong suit of `miniwatch`. Use something else.
-
-+ Because `stat` will peg one core of your CPU if it blindly runs in a loop, waiting for changes, `miniwatch` actually sleeps for a full second before checking the file each time.
++ Because `stat` will peg one core of your CPU if it blindly runs in a loop, waiting for changes, `miniwatch` actually sleeps for a full second before checking the file/directory each time.
 
 + `miniwatch` assumes that time moves forward, and modified times will result in similar linear behaviour. This is naive in the extreme, but should work most of the time.
 
@@ -34,9 +34,11 @@ There are some caveats:
 
 I wanted a program to watch and react to changes, but didn't want to install something like `inotify`.
 
-So I spent a whopping five minutes building this.
+So I spent a whopping five minutes building the initial version of this:
 
 An explanation of the pathetically minimal code can be found [here](https://shatterealm.netlify.app/programming/2021_03_23_lets_build_a_file_watcher) if you're thinking of making it more robust.
+
+Update: The directory handling code is a little bit more verbose than what is explained above, using a basic key-value store. It is still drop-dead simple. It took me a further hour to implement, and isn't really handling errors, either.
 
 ---
 
--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+#define _XOPEN_SOURCE 500
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdbool.h>
@@ -6,7 +8,142 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 
+#include <ftw.h>
+#include <sys/stat.h>
+#include <sys/resource.h>
+
 // ./main THING_TO_WATCH -c ARGS
+
+struct map_pair {
+	char* key;
+	time_t value;
+};
+
+struct map {
+	struct map_pair* cells;
+	size_t map_length;
+};
+
+bool map_init(struct map* m);
+ssize_t map_has_key(struct map* m, const char* key);
+bool map_set_key(struct map* m, const char* key, time_t value);
+bool map_remove_key(struct map* m, const char* key);
+time_t map_get_key(struct map* m, const char* key);
+void map_free(struct map* m);
+
+bool map_init(struct map* m) {
+	m->map_length = 0;
+	m->cells = calloc(1, sizeof(struct map_pair));
+	if(!m->cells) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+void map_free(struct map* m) {
+	for(size_t i = 0; i < m->map_length; i++) {
+		if(m->cells[i].key != NULL) {
+			free(m->cells[i].key);
+		}
+	}
+	free(m->cells);
+}
+
+ssize_t map_has_key(struct map* m, const char* key) {
+	for(size_t i = 0; i < m->map_length; i++) {
+		if(m->cells[i].key != NULL) {
+			if(strcmp(m->cells[i].key, key) == 0) {
+				return i;
+			}
+		}
+	}
+
+	return -1;
+}
+
+bool map_set_key(struct map* m, const char* key, time_t value) {
+	if(map_has_key(m, key) > -1) {
+		// Overwrite the key
+		size_t position = (size_t)map_has_key(m, key);
+		m->cells[position].value = value;
+		return true;
+	} else {
+		// New key
+
+		// Check if there is a space:
+		for(size_t i = 0; i < m->map_length; i++) {
+			if(m->cells[i].key == NULL) {
+				m->cells[i].key = strdup(key);
+				m->cells[i].value = value;
+				return true;
+			}
+		}
+
+		// Reallocation necessary:
+		struct map_pair* tmp = realloc(m->cells, (m->map_length + 2) * sizeof(struct map_pair));
+		if(!tmp) {
+			return false;
+		}
+		m->cells = tmp;
+		m->map_length++;
+		m->cells[m->map_length - 1].key = strdup(key);
+		m->cells[m->map_length - 1].value = value;
+		return true;
+	}
+
+	return false;
+}
+
+bool map_remove_key(struct map* m, const char* key) {
+	if(map_has_key(m, key) > -1) {
+		size_t position = (size_t)map_has_key(m, key);
+
+		free(m->cells[position].key);
+		m->cells[position].key = NULL;
+
+		m->cells[position].value = 0;
+		return true;
+	}
+
+	return false;
+}
+
+time_t map_get_key(struct map* m, const char* key) {
+	size_t position = (size_t)map_has_key(m, key);
+	return m->cells[position].value;
+}
+
+struct map files_map;
+
+bool files_modified = false;
+
+int
+file_check(fpath, sb, tflag, ftwbuf)
+	const char* fpath;
+	const struct stat* sb;
+	int tflag;
+	struct FTW* ftwbuf;
+{
+	// Only care about regular files...
+	if(tflag == FTW_F) {
+		// Is this a new file?
+		if(map_has_key(&files_map, fpath) == -1) {
+			files_modified = true;
+		} else {
+			// Has the file been modified?
+			size_t position = (size_t)map_has_key(&files_map, fpath);
+			if(difftime(map_get_key(&files_map, fpath), sb->st_mtime) < 0) {
+				files_modified = true;
+			}
+		}
+
+		// Update the file listing
+		map_set_key(&files_map, fpath, sb->st_mtime);
+	}
+
+	return 0;
+}
 
 int main(int argc, char* argv[]) {
 	if(argc < 2) {
@@ -36,39 +173,117 @@ int main(int argc, char* argv[]) {
 	}
 
 	struct stat sfile;
-	time_t old_time;
-	time_t current_time;
-	time(&old_time);
-	time(&current_time);
+
+	// Can we even access it?
+	if(stat(filename, &sfile) != 0) {
+		return 1;
+	}
 
 	pid_t pid;
 	int status;
 
-	while(true) {
-		// Check if file/dir modified...
-		stat(filename, &sfile);
-		current_time = sfile.st_mtime;
-
-		if(difftime(old_time, current_time) < 0) {
-			old_time = current_time;
-
-			if((pid = fork()) < 0) {
-				// Fork fail.
-				return 1;
+	// Check if filename is a directory...
+	if(S_ISDIR(sfile.st_mode)) {
+		// Get a reasonable file descriptor limit
+		// for when we're walking our tree.
+		struct rlimit rlp = {0};
+		size_t limit = 0;
+		if(getrlimit(RLIMIT_NOFILE, &rlp) == 0) {
+			// Set to soft limit
+			limit = rlp.rlim_cur / 4;
+			if(limit >= rlp.rlim_max) {
+				// Set to reasonable guess
+				limit = 20;
 			}
-			else if(pid == 0) {
-				// Run our command!
-				execvp(*array, array);
-			}
-			else {
-				// Wait until our child (execvp) terminates...
-				while(wait(&status) != pid) {
-					old_time = current_time;
-				}
-			}		
+		} else {
+			// Set to reasonable guess
+			limit = 20;
 		}
 
-		// Calling stat in an infinite loop will peg the CPU.
-		sleep(1);
+		// Initialise the file map
+		if(!map_init(&files_map)) {
+			return 1;
+		}
+
+		// Fill the initial data
+		files_modified = false;
+		if(nftw(filename, file_check, limit, 0) == -1) {
+			return 1;
+		}
+
+		// Loop...
+		while(true) {
+			// Check for modifications...
+			files_modified = false;
+			if(nftw(filename, file_check, limit, 0) == -1) {
+				return 1;
+			}
+
+			// Check for file deletions...
+			for(size_t i = 0; i < files_map.map_length; i++) {
+				if(files_map.cells[i].key != NULL) {
+					if(stat(files_map.cells[i].key, &sfile) != 0) {
+						map_remove_key(&files_map, files_map.cells[i].key);
+						files_modified = true;
+					}
+				}
+			}
+
+			// If files were modified, run command & wait
+			if(files_modified) {
+				files_modified = false;
+
+				if((pid = fork()) < 0) {
+					// Fork fail.
+					return 1;
+				}
+				else if(pid == 0) {
+					// Run our command!
+					execvp(*array, array);
+				}
+				else {
+					// Wait until our child (execvp) terminates...
+					while(wait(&status) != pid) {
+						files_modified = false;
+					}
+				}
+			}
+
+			// Calling stat in an infinite loop will peg the CPU.
+			sleep(1);
+		}
+	} else {
+		time_t old_time;
+		time_t current_time;
+		time(&old_time);
+		time(&current_time);
+
+		while(true) {
+			// Check if file/dir modified...
+			stat(filename, &sfile);
+			current_time = sfile.st_mtime;
+
+			if(difftime(old_time, current_time) < 0) {
+				old_time = current_time;
+
+				if((pid = fork()) < 0) {
+					// Fork fail.
+					return 1;
+				}
+				else if(pid == 0) {
+					// Run our command!
+					execvp(*array, array);
+				}
+				else {
+					// Wait until our child (execvp) terminates...
+					while(wait(&status) != pid) {
+						old_time = current_time;
+					}
+				}		
+			}
+
+			// Calling stat in an infinite loop will peg the CPU.
+			sleep(1);
+		}
 	}
 }
